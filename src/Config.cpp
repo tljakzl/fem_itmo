7a0d9da657d8b6dee90fe0defc5015f09a1075a8
Anatoliy Kozlov <tljakzl2@gmail.com>
2021-06-13 13:33:20 +0300

    fix compile

--- src/Config.cpp
+++ src/Config.cpp
@@ -1,49 +1,49 @@
 #include "Config.h"
 
 std::vector<std::function<double(Point)>> Config::BCOND1_FUNCS = {
-	[&](Point p) {
+	[](Point p) {
 		return p.x + p.y + 1;
 	}
 };
 
 std::vector<std::function<double(Point)>> Config::BCOND2_FUNCS = {
-	[&](Point p) {
+	[](Point p) {
 		return p.x + p.y;
 	}
 };
 
 std::vector<std::function<double(Point)>> Config::BCOND3_FUNCS = {
-	[&](Point p) {
+	[](Point p) {
 		return p.x + p.y;
 	}
 };
 
 std::vector<std::function<double(Point)>> Config::BCOND3_RP_FUNCS = {
-	[&](Point p) {
+	[](Point p) {
 		return p.x + p.y;
 	}
 };
 
 std::vector<std::function<double(Point)>> Config::RP_FUNCS = {
-	[&](Point p) {
+	[](Point p) {
 		return p.x + p.y;
 	}
 };
 
 std::vector<std::function<double(Point)>> Config::LAMBDA_FUNCS = {
-	[&](Point p) {
+	[](Point p) {
 		return p.x + p.y;
 	}
 };
 
 std::vector<std::function<double(Point)>> Config::GAMMA_FUNCS = {
-	[&](Point p) {
+	[](Point p) {
 		return p.x + p.y;
 	}
 };
 
 std::function<double(Point)> Config::ZERO_FUNC = {
-	[&](Point p) {
+	[](Point p) {
 		return 0;
 	}
 };
